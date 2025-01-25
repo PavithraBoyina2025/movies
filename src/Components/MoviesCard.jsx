d36f2923a0443c600d7b1802c5a3447b1d79e947
PavithraBoyina2025 <boyinapavithra@gmail.com>
2025-01-26 01:19:38 +0530

    changed icon

--- src/Components/MoviesCard.jsx
+++ src/Components/MoviesCard.jsx
@@ -18,7 +18,7 @@ function MoviesCard({movie, poster_path, name, AddToWatchList, RemoveFromWatchLi
     <div className='h-[40vh] w-[150px] rounded-xl flex flex-col justify-between items-end hover:scale-110 duration-300 hover:cursor' style={{backgroundRepeat:'no-repeat', backgroundSize:'cover', backgroundPosition:'center', backgroundImage:`url(https://image.tmdb.org/t/p/original/${poster_path})`}}>
 
      {Contain(movie) ? (<div onClick={()=>RemoveFromWatchList(movie)} 
-     className='m-3 flex justify-center h-8 w-8 items-center rounded-lg text-2xl font-bold bg-white/70 p-1'>&#x274C;</div>
+     className='m-3 flex justify-center h-8 w-8 items-center rounded-lg text-2xl font-bold bg-white/70 p-1'>&#128151;</div>
     ) : (
      <div onClick={()=>AddToWatchList(movie)} 
      className='m-3 flex justify-center h-8 w-8 items-center rounded-lg text-2xl font-bold bg-white/70 p-1'>&#x2661;</div>
